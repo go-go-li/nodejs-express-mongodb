ddb0949cb1e789841cd0659a700455f6ffd5c4c3
go-go-li <83599414+go-go-li@users.noreply.github.com>
2023-05-25 16:33:49 +0200

    1 file modified

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
-    console.log(process.env.MONGODB_URI);
-
     mongoose.set('strictQuery', false);
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Database connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log('123');
     console.log(error);
   }
 };
